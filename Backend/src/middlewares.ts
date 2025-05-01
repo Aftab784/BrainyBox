c4da0f1227902110993f3eed9bd71eaf10dfe19a
Aftab784 <af227656@gmail.com>
2025-05-01 16:05:49 +0500

    Backend Done

--- Backend/src/middlewares.ts
+++ Backend/src/middlewares.ts
@@ -1,10 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
+declare module "express" {
+  export interface Request {
+    userId?: string;
+  }
+}
 export const usermiddleware = (req: Request, res: Response, next: NextFunction): void => {
     try {
-        const token = req.headers["token"] as string;
-    
+        const token = req.headers['token'] as string;
         if (!token) {
           res.status(401).json({ message: "Token not provided" });
           return;
@@ -23,3 +27,18 @@ export const usermiddleware = (req: Request, res: Response, next: NextFunction):
         res.status(403).json({ message: "Invalid or expired token" });
       }
 };
+
+// export const usermiddleware = async (req, res, next) => {
+//   const token = req.headers.authorization?.split(" ")[1];
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_USER_SECRET) as { id: string };
+//     req.userId = decoded.id;
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ message: "Invalid token" });
+//   }
+// };
