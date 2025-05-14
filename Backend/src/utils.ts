19c8b893b5b849078a6a17a3ad9a9c4a548d4586
Aftab784 <aftab@AftabAsif>
2025-05-14 12:35:50 +0500

    Button Component Added

--- Backend/src/utils.ts
+++ Backend/src/utils.ts
@@ -1,10 +1,15 @@
 export function random(len: number){
-    let options = "skkalpcw04422090tngdlaftab";
-    let length = options.length; 
-    let ans = "";
+    const numberPool= "0123456789";
+    const letterPool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let ans = ""
 
-    for(let i = 0; i < length; i++){
-       ans += options[Math.floor((Math.random() * length))]
+     for (let i = 0; i < len; i++) {
+        // 70% chance of picking a digit
+        if (Math.random() < 0.6) {
+            ans += numberPool[Math.floor(Math.random() * numberPool.length)];
+        } else {
+            ans += letterPool[Math.floor(Math.random() * letterPool.length)];
+        }
     }
     return ans;
 }
